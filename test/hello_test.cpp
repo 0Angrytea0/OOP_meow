28fb37b113790bbc2c34288a3cca72ed0c0c92d8
0Angrytea0 <viktoriyasokolova04@gmail.com>
2023-09-18 19:03:13 +0000

    тесты

--- test/hello_test.cpp
+++ test/hello_test.cpp
@@ -1,14 +1,123 @@
 #include <gtest/gtest.h>
+#include <string>
+#include "lib.h" 
+using namespace std;
 
 
-TEST(HelloTest, BasicAssertions) {
+
+TEST(ExampleOfStandartUsageTest1, BasicAssertions) {
   // arrange 
-  int first = 2;
-  int second = 5;
+  string first = "asda";
 
   // act
-  int sum = first + second;
-
+  char vowel[] = {'a', 'e', 'i', 'o', 'y', 'u'};
+  int k = 0; 
+  int result = kolvo(k, first, vowel);
+  
   // assert
-  EXPECT_EQ(sum, 7);
+  EXPECT_EQ(result, 2);
+}
+
+TEST(ExampleOfStandartUsageTest2, BasicAssertions) {
+  // arrange 
+  string first = "aa e i o y u";
+
+  // act
+  char vowel[] = {'a', 'e', 'i', 'o', 'y', 'u'};
+  int k = 0; 
+  int result = kolvo(k, first, vowel);
+  
+  // assert
+  EXPECT_EQ(result, 7);
+}
+
+TEST(ExampleOfStandartUsageTest3, BasicAssertions) {
+  // arrange 
+  string first = "aeaeae ia is su s";
+
+  // act
+  char vowel[] = {'a', 'e', 'i', 'o', 'y', 'u'};
+  int k = 0; 
+  int result = kolvo(k, first, vowel);
+  
+  // assert
+  EXPECT_EQ(result, 10);
+}
+
+TEST(ExampleOfStandartUsageTest4, BasicAssertions) {
+  // arrange 
+  string first = "a";
+
+  // act
+  char vowel[] = {'a', 'e', 'i', 'o', 'y', 'u'};
+  int k = 0; 
+  int result = kolvo(k, first, vowel);
+  
+  // assert
+  EXPECT_EQ(result, 1);
+}
+
+TEST(ExampleOfStandartUsageTest5, BasicAssertions) {
+  // arrange 
+  string first = "a   asdf ef   efa";
+
+  // act
+  char vowel[] = {'a', 'e', 'i', 'o', 'y', 'u'};
+  int k = 0; 
+  int result = kolvo(k, first, vowel);
+  
+  // assert
+  EXPECT_EQ(result, 5);
+}
+
+TEST(ExampleOfStandartUsageTest6, BasicAssertions) {
+  // arrange 
+  string first = " ";
+
+  // act
+  char vowel[] = {'a', 'e', 'i', 'o', 'y', 'u'};
+  int k = 0; 
+  int result = kolvo(k, first, vowel);
+  
+  // assert
+  EXPECT_EQ(result, 0);
+}
+
+TEST(ExampleOfStandartUsageTest7, BasicAssertions) {
+  // arrange 
+  string first = " a ";
+
+  // act
+  char vowel[] = {'a', 'e', 'i', 'o', 'y', 'u'};
+  int k = 0; 
+  int result = kolvo(k, first, vowel);
+  
+  // assert
+  EXPECT_EQ(result, 1);
+}
+
+TEST(ExampleOfStandartUsageTest8, BasicAssertions) {
+  // arrange 
+  string first = "       o";
+
+  // act
+  char vowel[] = {'a', 'e', 'i', 'o', 'y', 'u'};
+  int k = 0; 
+  int result = kolvo(k, first, vowel);
+  
+  // assert
+  EXPECT_EQ(result, 1);
+}
+
+TEST(ExampleOfStandartUsageTest9, BasicAssertions) {
+  // arrange 
+  string first = "                  ";
+
+  // act
+  char vowel[] = {'a', 'e', 'i', 'o', 'y', 'u'};
+  int k = 0; 
+  int result = kolvo(k, first, vowel);
+  
+  // assert
+  EXPECT_EQ(result, 0);
 }
