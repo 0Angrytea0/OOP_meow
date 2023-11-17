85aadaddec21fff540cd15154add80bc3fd796aa
0Angrytea0 <116645263+0Angrytea0@users.noreply.github.com>
2023-11-18 00:48:03 +0300

    Update octagon.h

--- include/octagon.h
+++ include/octagon.h
@@ -9,7 +9,7 @@ class Octagon: public Figure
     friend std::ostream& operator<<(std::ostream& os, const Octagon& t);
     friend std::istream& operator>>(std::istream& is, Octagon& t);
 public:
-    Octagon(): oct{Point(), Point(), Point(), Point(), Point(), Point(), Point(), Point()} {};
+    Octagon(): oct{Point(), Point(), Point(), Point(), Point(), Point(), Point(), Point()}{};
     Octagon(Point& ver1, Point& ver2, Point& ver3, Point& ver4, Point& ver5, Point& ver6, Point& ver7, Point& ver8);
     Octagon(const Octagon &other);
 
@@ -23,11 +23,12 @@ public:
     ~Octagon(){};
 
     Point oct[8]{};
+    size_t size{8};
 };
 
 inline std::ostream& operator<<(std::ostream& os, const Octagon& f){
     os << "Octagon's coordinates:"<< std::endl;
-    for(size_t i = 0; i < 8; ++i){
+    for(size_t i = 0; i < f.size; ++i){
         os << f.oct[i] << std::endl;
     }
     return os;
@@ -36,14 +37,14 @@ inline std::ostream& operator<<(std::ostream& os, const Octagon& f){
 inline std::istream& operator>>(std::istream& is, Octagon& f){
     std::cout << "Please enter the coordinates of the vertices of the octagon:" << std::endl;
     std::cout << "order [x,y], separated by spaces, the first vertex is the upper left, go clockwise order: " << std::endl;
-    for (size_t i = 0; i < 8; ++i) {
+    for (size_t i = 0; i < f.size; ++i) {
         is >> f.oct[i];
         if (is.fail()) {
             throw std::invalid_argument("Input must be a number");
         }
     }
-    for (int i = 0; i < 8; i++) {
-        for (int j = i + 1; j < 8; j++) {
+    for (int i = 0; i < f.size; i++) {
+        for (int j = i + 1; j < f.size; j++) {
             if (f.oct[i] == f.oct[j]) {
                 throw std::logic_error("Same coordinates for different points");
             }
